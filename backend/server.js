8912ebdd01770ee07bf355b6db163944cf95477a
Ayush Mehrotra <ayushmehrotra099@gmail.com>
2024-03-28 09:47:36 -0700

    removed next & added node and express

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.listen(3000, () => {
+  console.log("Server is on 3000");
+});
