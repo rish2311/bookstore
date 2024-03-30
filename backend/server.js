7636ef9b7e0c77ac7720a08b85ede1f66ebebe22
Ayush Mehrotra <ayushmehrotra099@gmail.com>
2024-03-29 23:36:38 -0700

    signup route working

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const app = express();
 
+const bodyParser = require("body-parser");
+const userRoutes = require("./routes/userRoutes");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use("/api/v1", userRoutes);
+
 app.get("/", (req, res) => {
-  res.send("Hello");
+  res.status(200).json({
+    success: true,
+    message: "Server is working fine",
+  });
 });
 
 app.listen(3000, () => {
