da773834e1bed04fd22e1be29568e8c8061361db
mrayushmehrotra <ayushmehrotra099@gmail.com>
2024-05-17 18:32:43 +0530

    added pass hashing and signed jwt on login

---
+++ README.md
@@ -0,0 +1,7 @@
+# This is a online book selling platform
+
+(under developement)
+
+<img src="https://i.pinimg.com/originals/c7/63/55/c76355a55c254c02537c78fbee6ec3df.jpg" alt="demofrontend png">
+
+<h4>this is how the frontend will look like in future</h4>
--- backend/Controller/userController.js
+++ backend/Controller/userController.js
@@ -1,10 +1,14 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
+const bcrypt = require("bcrypt"); // Import bcrypt for password hashing
+const jwt = require("jsonwebtoken");
 
 exports.registerUser = async (req, res) => {
   try {
     const { username, password, name } = req.body;
 
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
     const searchUser = await prisma.user.findMany({
       where: {
         username: username,
@@ -17,7 +21,7 @@ exports.registerUser = async (req, res) => {
       data: {
         name: name,
         username: username,
-        password: password,
+        password: hashedPassword,
       },
     });
     res.status(201).send("User created successfully");
@@ -30,21 +34,48 @@ exports.registerUser = async (req, res) => {
 // Signup endpoint
 exports.loginUser = async (req, res) => {
   const { username, password } = req.body;
-  const getUser = await prisma.user.findMany({
-    where: {
-      username: username,
-      password: password,
-    },
-  });
-  if (getUser.length === 0) {
-    res.status(401).json({
+  if (!username || !password) {
+    return res.status(422).json({
       success: false,
-      message: "User not exists or incorrect username or password",
-    });
-  } else {
-    res.status(200).json({
-      success: true,
-      message: "Login successfully",
+      error: "Please Fill email or password",
     });
   }
+
+  await prisma.user
+    .findUniqueOrThrow({
+      where: {
+        username: username,
+      },
+    })
+    .then((savedUser) => {
+      if (!savedUser) {
+        return res.status(500).json({
+          success: false,
+          error: "Invalid email or password",
+        });
+      }
+      bcrypt.compare(password, savedUser.password).then((doMatch) => {
+        if (doMatch) {
+          const token = jwt.sign(
+            { _id: savedUser._id },
+            process.env.JWT_SECRET
+          );
+          const { _id, name, email } = savedUser;
+          res.json({
+            success: true,
+            token,
+            savedUser,
+            message: "signed in successfully",
+          });
+        } else {
+          return res.status(522).json({
+            success: false,
+            error: "Invalid Email or password",
+          });
+        }
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
